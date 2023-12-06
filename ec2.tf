311a4f8399d126b140227bee1ad56355c4ab9804
Purnima-02 <140998496+Purnima-02@users.noreply.github.com>
2023-12-06 13:12:46 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 #creating ec2 instance ec2.tf
 resource "aws_instance" "instance1"{
-ami= "ami-09b91b4166e3eda84"
+ami= "ami-0d92749d46e71c34c"
 instance_type="t2.small"
-key_name="kesava"
+key_name="master"
 vpc_security_group_ids=["${aws_security_group.demosg.id}"]
 subnet_id=aws_subnet.public-subnet-1.id
 associate_public_ip_address=true
