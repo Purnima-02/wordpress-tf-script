bcb15eade4505ca5aa7021401f24119ad2dbad23
Purnima-02 <140998496+Purnima-02@users.noreply.github.com>
2023-12-06 13:14:42 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -3,7 +3,7 @@
   vpc_id                  = aws_vpc.demovpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "eu-west-2a"
+  availability_zone       = "ap-south-1a"
   tags = {
     Name = "Web Subnet 1"
   }
@@ -12,7 +12,7 @@ resource "aws_subnet" "public-subnet-2" {
   vpc_id                  = aws_vpc.demovpc.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "eu-west-2b"
+  availability_zone       = "ap-south-1b"
   tags = {
     Name = "Web Subnet 2"
   }
